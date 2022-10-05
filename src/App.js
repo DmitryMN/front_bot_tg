7f955965f74e546dcacd0c5d4d8825fd86083135
MalenkovDN <m18dm.dm@gmail.com>
2022-10-05 11:32:44 +0300

    refactor errors in build

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import { useTelegram } from './hooks/useTelegram';
 import { Header } from './components/Header/Header';
@@ -7,13 +7,18 @@ import { Header } from './components/Header/Header';
 
 function App() {
 
-  const { onToggleButton } = useTelegram();
+  const {onToggleButton, tg } = useTelegram();
+
+  useEffect(() => {
+    tg.ready();
+  }, [tg]);
 
 
   return (
     <div className="App">
       <button onClick={onToggleButton}>toggle</button>
       <Header />
+      <div>your price</div>
     </div>
   );
 }
